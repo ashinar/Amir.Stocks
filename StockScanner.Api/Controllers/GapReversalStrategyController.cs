fc319023d7dfd02fd16c4e203e0214f36cb27e63
Amir Shinar <amirs@top4.com>
2026-08-19 09:51:04 +0300

    first commit

---
+++ StockScanner.Api/Controllers/GapReversalStrategyController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StockScanner.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GapReversalStrategyController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new
+            {
+                Message = "Stock Scanner API is running"
+            });
+        }
+    }
+}
